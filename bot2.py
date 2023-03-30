7639e40ca310fcb0ba6fed14ac9e0fe3f952f566
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-30 05:52:38 -0300

    commit

--- bot2.py
+++ bot2.py
@@ -1,12 +1,13 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+service = Service(ChromeDriverManager().install())
+
 def account_info():
-    with open('account_info.txt' 'r') as f:
+    with open('account_info.txt', 'r') as f:
         info = f.read().split()
         email = info[0]
         password = info[1]
@@ -16,9 +17,22 @@ email, password = account_info()
 
 tweet = 'Mais um dia sem o Bolsonaro preso.'
 
-Options = Options()
-Options.add_argument("start-maximized")
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 
 driver.get("https://twitter.com/login")
+
+time.sleep(1)
+
+email_xpath = '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[5]/label/div/div[2]/div/input'
+password_xpath = ''
+
+input_email = driver.find_element(By.XPATH, email_xpath)
+input_email.send_keys(email)
+
+button_email = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[6]')
+button_email.click()
+
+input_password = driver.find_element(By.XPATH, '')
+
+
